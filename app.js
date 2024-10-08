92d577f5f056dfc7b259361b3839da7b09385959
SyedBashar <MhuammadBashar@gmail.com>
2024-10-08 17:06:11 +0500

    Done

---
+++ app.js
@@ -0,0 +1,4 @@
+alert("I am 15 years old ")
+alert("You have visited this site 14 times")
+document.write(" <h2>My birth Year is 2007 <br>Data type of my ddeclared variable is number</h2>")
+document.write("jhon Doe ordered 5 t-shirt (s) on xyz clothing store")
